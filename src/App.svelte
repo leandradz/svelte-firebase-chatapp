76d9860f2c2361b79e480b7c2f9d32c0183d463d
Leticia de Andrade <leticia_andrade94@hotmail.com>
2022-07-24 20:05:01 -0300

    feat: include google authentication

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,36 @@
 <script>
+  import { auth, googleProvider } from "./firebase";
+  import { authState } from "rxfire/auth";
+
+  let user;
+
+  const unsubscribe = authState(auth).subscribe((u) => (user = u));
+
+  function login() {
+    auth.signInWithPopup(googleProvider);
+  }
 </script>
 
-<main>Hello World</main>
+<main>
+  {#if user}
+    Logged in as {user.uid}
+  {:else}
+    <button on:click={login}>
+      <i class="fa fa-google" />
+      Sing in with Google
+    </button>
+  {/if}
+</main>
 
 <style>
+  main {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    max-width: 400px;
+    max-height: 500px;
+  }
 </style>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,20 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APP_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDQu-3CVcsLdbTT222qcqkjJU7PHpLMgHA",
+  authDomain: "svelte-firebase-chatapp.firebaseapp.com",
+  projectId: "svelte-firebase-chatapp",
+  storageBucket: "svelte-firebase-chatapp.appspot.com",
+  messagingSenderId: "857296843784",
+  appId: "1:857296843784:web:d366fbcb7c53c2500c8dbd",
 };
 
+firebase.initializeApp(firebaseConfig);
+
 export const auth = firebase.auth();
-export const googleProvider = new firebase.auth.GoogleAuthProvider()
-export const db = firebase.firestore()
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export const db = firebase.firestore();
